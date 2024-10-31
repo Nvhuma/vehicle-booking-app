275875253c9cd57adeaa7c2f072fe676449faed3
Nbvuma <nhlayisekobvuma7@gmail.com>
2024-10-31 16:07:34 +0200

    created route for payments

--- api/Interfaces/ICardDetailsService.cs
+++ api/Interfaces/ICardDetailsService.cs
@@ -1,12 +1,25 @@
+
+
+namespace api.Interfaces
+
+{
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Models;
 
-namespace api.Interfaces
-{
-    public class ICardDetailsService
+
+    public interface ICardDetailsService
+    
+
+
+
+ 
     {
-        
+         Task<CardDetails> GetCardDetailsByIdAsync(int Id, string UserID);
+
+				 Task UpdateCardAsync (CardDetails card);
     }
 }
+    
---
+++ api/Services/CardDetailsService.cs
@@ -0,0 +1,40 @@
+
+
+namespace api.Services
+
+{
+	using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+	using api.Data;
+	using api.Interfaces;
+	using api.Models;
+	using Microsoft.EntityFrameworkCore;
+
+	public class CardDetailsService : ICardDetailsService
+	{
+				private readonly ApplicationDBContext _context;
+
+		public  CardDetailsService(ApplicationDBContext context)
+		{
+			_context = context ?? throw new ArgumentNullException(nameof(context));
+		
+		}
+
+		
+		public async Task<CardDetails> GetCardDetailsByIdAsync(int Id, string UserID)
+		{
+			
+			 return await _context.CardDetails
+								.FirstOrDefaultAsync(c => c.Id == Id && c.UserID == UserID);
+
+		}
+
+		public async Task UpdateCardAsync(CardDetails card)
+		{
+			 _context.CardDetails.Update(card);
+            await _context.SaveChangesAsync();
+		}
+	}
+}
